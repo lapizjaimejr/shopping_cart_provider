0dccb1416a1e736a167adcb75b8b49fba562e32c
Jai <jaime.lapiz@usjr.edu.ph>
2022-07-21 11:41:04 +0800

    added initial component of DataClass

---
+++ lib/data_class.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DataClass extends ChangeNotifier {
+  int _x = 0;
+  int get x => _x;
+
+  void incrementX() {
+    _x++;
+    notifyListeners();
+  }
+}
